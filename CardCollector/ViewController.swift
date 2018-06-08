04a7a1bdf1a09eb47ead4b9cd071202c7b3b09be
David Kendlbacher <david@davids-macbook-pro.local>
2018-06-08 23:25:48 +0200

    Initial Commit

---
+++ CardCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardCollector
+//
+//  Created by David Kendlbacher on 08.06.18.
+//  Copyright © 2018 David Kendlbacher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
